941815a282323a50be8e97521827a674f86fc7d6
ridleo201 <ridleo201@gmail.com>
2022-01-26 20:45:50 -0500

    firt-commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 'hello';
 
 // Crea una variable booleana:
 const nuevoBool = null;
